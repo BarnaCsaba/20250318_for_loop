c86d0e280f44c1c4b58bfc2b924ffcab33d00ebd
Csib_Csab <barna.csaba@diak.szbi-pg.hu>
2025-03-18 10:42:31 +0100

    pushi

---
+++ for.js
@@ -0,0 +1,18 @@
+
+for (let index = 1; index < 21; index++) {
+    console.log(index, index**2, Math.sqrt(index));
+    
+}
+var a = "🤩";
+
+for (let index = 0; index < 10; index++) {
+    console.log(a.repeat(index));
+    
+}
+
+for (let index = 99; index > 0; index--) {
+    if (index % 3 == 0)
+        {
+            console.log(index);
+        }
+}
